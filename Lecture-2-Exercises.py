d0987a9c9a8e3da7c57401c2544a56a08a353b54
Søren Howe Gersager <syrelyre@gmail.com>
2014-09-11 13:31:46 +0200

    added lecture2 py

---
+++ Lecture-2-Exercises.py
@@ -0,0 +1,192 @@
+
+# coding: utf-8
+
+# # Exercises
+
+# ## Exercise 3 (Natural Language Processing)
+# 
+
+# In[40]:
+
+import nltk
+from nltk.book import *
+get_ipython().magic(u'matplotlib inline')
+
+
+
+# In[23]:
+
+text3.concordance("rain")
+
+
+# In[16]:
+
+text1.similar("good")
+
+
+# In[30]:
+
+text4.common_contexts(["good","bad"])
+
+
+# In[28]:
+
+text3.dispersion_plot(["good", "evil"])
+
+
+# In[55]:
+
+fdist1 = nltk.FreqDist(text2)
+fdist1.most_common(75)
+
+
+# In[56]:
+
+# used 50 since i was unable to see the words on the x axis with 75
+fdist1.plot(50,cumulative=True)
+
+
+# #NLPP Exercises 1.8
+
+# ### 1.8.1
+
+# In[57]:
+
+6 / (3 + 1)
+
+
+# ### 1.8.2
+
+# In[58]:
+
+26 ** 100
+
+
+# ### 1.8.3 What happens when you type ["Monty", "Python"]*20
+# A single list is created with 20 "Monty", "Pythons"
+# 
+
+# In[60]:
+
+["Monty", "Python"]*20
+
+
+# ### 1.8.4 How many words and distinct words in text2?
+
+# In[66]:
+
+print("total words: {0}".format(len(text2)))
+print("total distinct words: {0}".format(len(set(text2))))
+
+
+# ### 1.8.5 Which genre is more lexical diverse?
+# We are using the definition of lexical diversity from the MTSW book and the new NLTK book ( len(set(text)) / len(text) ) because of a discrepancy from the old and the new book. According to the table it seems Humour is more lexical diverse with a value of 0.231 where Fiction: Romance only has a value of 0.121
+
+# ### 1.8.6 Produce a dispersion plot of the four main protagonists in Sense and Sensibility
+
+# In[67]:
+
+text2.dispersion_plot(["Elinor", "Marianne", "Edward", "Willoughby"])
+
+
+# We can observe that the females play a much larger role in the novel than the males and are mentioned very often.
+# An estimate from the plot is that the couples are: (Elinor, Edward) and (Marianne, Wiloughby)
+
+# ### 1.8.7 Find collocations in text 5
+
+# In[68]:
+
+text5.collocations()
+
+
+# ### 1.8.8 Explain len(set(text4))
+
+# The set function in python converts the sequence to a set thus removing duplicates.
+# The len function counts the number of elements in the sequence.
+
+# ### 1.8.12
+# The first is a slice expression which slices from element 6 to 12, the second expression extracts the second element in the list, the second expression will typically be more relevant for NLP
+
+# In[72]:
+
+print("Monty Python"[6:12])
+print(["Monty", "Python"][1])
+
+
+# ### 1.8.15
+
+# In[74]:
+
+sorted([w for w in set(text5) if w.startswith("b")])
+
+
+# ### 1.8.17
+
+# In[83]:
+
+print(text9.index("sunset"))
+print(text9[621:643])
+
+
+# ### 1.8.19
+# The second will give the larger sequence and this will always be the case. Because the set function is used before the list comprehension thus it removes lower word duplicates as done by the w.lower()
+
+# In[106]:
+
+print(len(sorted(set([w.lower() for w in text1]))))
+print(len(sorted([w.lower() for w in set(text1)])))
+
+
+# Another example
+
+# In[107]:
+
+words = ["boss", "Boss", "boss", "boss"]
+first = sorted(set([w.lower() for w in words]))
+second = sorted([w.lower() for w in set(words)])
+print(first)
+print(second)
+
+
+# ### 1.8.22
+
+# In[97]:
+
+four_letter_words = [w for w in text5 if len(w) == 4]
+fdist = FreqDist(four_letter_words)
+fdist.most_common()
+
+
+# ### 1.8.23
+
+# In[99]:
+
+for word in text6:
+    if word.isupper():
+        print(word,)
+
+
+# ### 1.8.25
+
+# In[101]:
+
+words = ['she', 'sells', 'sea', 'shells', 'by', 'the', 'sea', 'shore']
+print([w for w in words if w.startswith("sh") or len(w) > 4])
+
+
+# ### 1.8.26 What does the following Python code do? sum([len(w) for w in text1]) Can you use it to work out the average word length of a text?
+# It counts the sum of word lengths in the text1 text.
+# We can work out the average word length like this:
+
+# In[102]:
+
+sum([len(w) for w in text1])/len(text1)
+
+
+# ### 1.8.27 Define a function called vocab_size(text) that has a single parameter for the text, and which returns the vocabulary size of the text.
+
+# In[103]:
+
+def vocab_size(text):
+    return len(set(text))
+
