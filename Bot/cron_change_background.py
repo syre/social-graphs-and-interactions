3a5e337c395c18610cdc9dacfdaf4cb7a4cce038
Søren Howe Gersager <syrelyre@gmail.com>
2014-11-01 11:50:19 +0100

    cron for changing background

---
+++ Bot/cron_change_background.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from twitter_bot import *
+import random
+import time
+"""cron_change_background should start every 3 days in the cron tab"""
+
+random.seed()
+time.sleep(random.randint(0,60*15))
+
+img_urls = find_profile_background_images()
+
+set_profile_background_image(random.choice(img_urls))
