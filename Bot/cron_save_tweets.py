95e4601e46d39e1fec879124c477d907b8dad224
Søren Howe Gersager <syrelyre@gmail.com>
2014-11-04 16:19:46 +0100

    new cron job for machinelearning retweet

--- Bot/cron_save_tweets.py
+++ Bot/cron_save_tweets.py
@@ -5,7 +5,7 @@ import time
 """post_popular_retweet should start for each 8 hour + random delay (2h)
    in the cron tab"""
 random.seed()
-time.sleep(random.randint(0,60*60*2))
+#time.sleep(random.randint(0,60*60*2))
 
 home_timeline_tweets = get_home_timeline_tweets()
 for tweet in home_timeline_tweets:
