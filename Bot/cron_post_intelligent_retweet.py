044e97f7cf0287b5c422ae153c1fb6ee561a2aa0
Søren Howe Gersager <syrelyre@gmail.com>
2014-11-04 16:21:52 +0100

    new cron description in intelligent_retweet

--- Bot/cron_post_intelligent_retweet.py
+++ Bot/cron_post_intelligent_retweet.py
@@ -2,7 +2,7 @@
 from twitter_bot import *
 import random
 import time
-"""post_popular_retweet should start at 12:00 san fransisco time + random delay (2h)
+"""post_popular_retweet should start at 1:00 san fransisco time + random delay (2h)
    in the cron tab"""
 random.seed()
 time.sleep(random.randint(0,60*60*2))
